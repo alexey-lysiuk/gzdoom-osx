0478b9287cad0ec34d2aea8ea297a037b1d18093
Alexey Lysiuk <alexey.lysiuk@gmail.com>
2011-08-20 12:50:00 +0300

    Integrated latest changes from GZDoom SVN at revision 1246. ZDoom revision is 3283.

--- src/sdl/crashcatcher.c
+++ src/sdl/crashcatcher.c
@@ -8,12 +8,14 @@
 #include <sys/ucontext.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
 
 #ifdef __linux__
 #include <sys/prctl.h>
 #ifndef PR_SET_PTRACER
 #define PR_SET_PTRACER 0x59616d61
+#endif
+#else if defined (__APPLE__)
+#include <signal.h>
 #endif
 #endif
 
